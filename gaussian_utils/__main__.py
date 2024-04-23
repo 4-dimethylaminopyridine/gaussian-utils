1d15a027a7f2d41d8d16b17f84203a17facd6fff
Yiming Zhang <yiming.zhang2000@outlook.com>
2024-04-23 17:20:26 -0400

    refactor: rewrite gaussian input file generation

--- gaussian_utils/__main__.py
+++ gaussian_utils/__main__.py
@@ -1,6 +1,7 @@
 import argparse
 import pathlib
 from .conformer_searcher import ConformerSearcher
+from .gaussian_input_generator import GaussianInputGenerator
 
 def search_conformers_subcommand(args):
 
@@ -13,6 +14,18 @@ def search_conformers_subcommand(args):
 
     searcher = ConformerSearcher(input_file)
     searcher.generate_results(dest_dir)
+
+def generate_gs_inputs_subcommand(args):
+
+    config_file = pathlib.Path(args.config_file)
+    dest_dir = pathlib.Path(args.dest_dir)
+
+    # make sure the config file exist
+    if not config_file.is_file():
+        raise FileNotFoundError(f'{config_file} does not exist!')
+
+    generator = GaussianInputGenerator(config_file)
+    generator.generate_gaussian_input(dest_dir)
 
 def main():
 
@@ -35,6 +48,21 @@ def main():
         'dest_dir', metavar='<destination_dir>',
         help='path to the output directory',
     )
+
+    # generate-gs-inputs subcommand
+    subparser_generate_gs_inputs = subparsers.add_parser(
+        'generate-gs-inputs',
+        help='Generate Gaussian input files according to the config file.'
+    )
+    subparser_generate_gs_inputs.add_argument(
+        'config_file', metavar='<config_file>',
+        help='path to the config file',
+    )
+    subparser_generate_gs_inputs.add_argument(
+        'dest_dir', metavar='<destination_dir>',
+        help='path to the output directory',
+    )
+    subparser_generate_gs_inputs.set_defaults(func=generate_gs_inputs_subcommand)
 
 
     args = parser.parse_args()
---
+++ gaussian_utils/gaussian_input_generator.py
@@ -0,0 +1,129 @@
+import pathlib
+import yaml
+import rdkit.Chem
+import jinja2
+
+
+class GaussianInputGenerator:
+
+    _GAUSSIAN_GJF_TEMPLATE = '''{{ gaussian_route_section }}
+
+{{ gaussian_title_section }}
+
+{{ molecule_charge }} {{ molecule_spin }}
+{{ molecule_xyz }}
+
+{%- for section in optional_additional_sections %}
+{{ section }}
+
+{% endfor %}
+! This is a comment line to indicate necessary blank lines
+'''
+
+    def __init__(self, config_file_path):
+
+        self._config_file_path = pathlib.Path(config_file_path)
+
+    def _sdf_to_gjf(self, sdf_path, output_dir, additional_args):
+
+        """Generate a list of gjf files from a sdf file. Each entry in the sdf file
+        must contain a "ID" property.
+
+        Parameters
+        ----------
+        sdf_path : pathlib.Path
+            Path to the sdf file.
+        output_dir : pathlib.Path
+            Path to the output directory.
+        additional_args: dict
+            additional arguments required for template rendering.
+
+        Returns
+        -------
+        None.
+
+        """
+
+        # create directory
+        output_dir = pathlib.Path(output_dir)
+        output_dir.mkdir(parents=True)
+
+        reader = rdkit.Chem.SDMolSupplier(str(sdf_path), removeHs=False)
+        for mol in reader:
+            ID = mol.GetProp('ID')
+            molecule_xyz = rdkit.Chem.MolToXYZBlock(mol)
+            molecule_xyz = ''.join(molecule_xyz.splitlines(keepends=True)[2:]) # need to remove the first two lines
+
+            args = additional_args.copy()
+            args['molecule_xyz'] = molecule_xyz
+
+            self._render_template(
+                self._GAUSSIAN_GJF_TEMPLATE,
+                output_dir / f'ID_{ID}.gjf',
+                args,
+            )
+
+    def mol2_to_gjf(self, mol2_path, output_file, additional_args):
+
+        mol = rdkit.Chem.MolFromMol2File(str(mol2_file), removeHs=False)
+
+        molecule_xyz = rdkit.ChemMolToXYZBlock(mol)
+
+        # need to remove first 2 lines
+        molecule_xyz = ''.join(molecule_xyz.splitlines(keepends=True)[2:])
+
+        args = additional_args.copy()
+        args['molecule_xyz'] = molecule_xyz
+
+        self._render_template(
+            self._GAUSSIAN_GJF_TEMPLATE,
+            output_file,
+            args
+        )
+
+    def _render_template(self, template_str, dest_file, args):
+        jinja2_template = jinja2.Environment(
+            loader=jinja2.BaseLoader()
+        ).from_string(template_str)
+
+        with open(dest_file, 'w') as f:
+            f.write(jinja2_template.render(**args))
+
+    def generate_gaussian_input(self, output_dir):
+
+        output_dir = pathlib.Path(output_dir)
+
+        with self._config_file_path.open() as f:
+            yaml_data = yaml.safe_load(f)
+
+        for item in yaml_data['files']:
+
+            filename = item['filename']
+            output_name = item['output_name']
+            gaussian_title_section = item['gaussian_title_section']
+            gaussian_route_section = item['gaussian_route_section']
+            molecule_charge = item['molecule_charge']
+            molecule_spin = item['molecule_spin']
+
+            # can leave this blank
+            if 'optional_additional_sections' in item:
+                optional_additional_sections = item['optional_additional_sections']
+            else:
+                optional_additional_sections = []
+
+            template_args =  {
+                'gaussian_title_section': gaussian_title_section,
+                'gaussian_route_section': gaussian_route_section,
+                'molecule_charge': molecule_charge,
+                'molecule_spin': molecule_spin,
+                'optional_additional_sections': optional_additional_sections
+            }
+
+            for f in self._config_file_path.parent.glob(filename):
+                print(f'Converting {f}')
+                if f.suffix == '.sdf':
+                    self._sdf_to_gjf(f, f'{output_dir}/{output_name}', template_args)
+                elif f.suffix == '.mol2':
+                    self._mol2_to_gjf(f, f'{output_dir}/{output_name}.gjf', template_args)
+                else:
+                    raise ValueError(f'Unsupported file type: {f.suffix}')
